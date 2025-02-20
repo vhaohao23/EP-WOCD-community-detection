6de739abdf1bb12f8f3315b39c3ddf7836731c09
vhaohao23 <viethaodt123@gmail.com>
2025-02-20 15:26:31 +0700

    again

---
+++ github/test.cpp
@@ -0,0 +1,9 @@
+#include <cstdlib>  // For system()
+#include <iostream>  // For cout
+using namespace std;
+int main() {
+    for (int i = 0; i < 30; i++) {
+        system("D:\\Hao\\nckh\\EP-WOCD-community-detection\\github\\ep-wocd-NMI.exe");  // Runs x.exe 30 times
+    }
+    return 0;
+}
